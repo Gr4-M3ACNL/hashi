e968b9d2d32e89bc839296b217d9387c3bade4d0
meo <meo.prn@outlook.fr>
2025-01-06 17:48:58 +0100

    init: initialisation du projet

---
+++ src/main/java/fr/m3acnl/hashi/Hashi.java
@@ -0,0 +1,10 @@
+package fr.m3acnl.hashi;
+
+/**
+ * Main class of the Hashi game.
+ */
+public class Hashi {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/fr/m3acnl/hashi/HashiTest.java
@@ -0,0 +1,19 @@
+package fr.m3acnl.hashi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HashiTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
