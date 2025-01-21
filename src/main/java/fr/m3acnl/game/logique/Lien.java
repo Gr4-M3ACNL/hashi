44f34d42b2920197066d4e78bfb08873e39b24d6
Ge0mega <cognardluka@gmail.com>
2025-01-21 18:05:46 +0100

    docs: Ajout des commentaires

--- src/main/java/fr/m3acnl/game/logique/Lien.java
+++ src/main/java/fr/m3acnl/game/logique/Lien.java
@@ -1,42 +1,79 @@
 package fr.m3acnl.game.logique;
 
+/**
+ * Cette classe représente un lien entre 2 noeud,
+ * il connait les 2 noeud lié, son nombre de lien
+ * et son nombre de lien soluce
+ */
 public class Lien {
-
     private Noeud noeud1;
     private Noeud noeud2;
-    private int etat;
-    private int etatSoluce;
+    private int nbLien;
+    private int nbLienSoluce;
 
-    public Lien(Noeud n1, Noeud n2, int sol) {
-        noeud1 = n1;
-        noeud2 = n2;
-        etatSoluce = sol;
-        etat = 0;
+    /**
+     * Constructeur pour une nouvelle instance de Lien
+     * 
+     * @param n1 premier Noeud
+     * @param n2 deuxième Noeud
+     * @param sol le nombre de lien soluce
+     */
+    public Lien(Noeud n1, Noeud n2, int sol){
+        noeud1=n1;
+        noeud2=n2;
+        nbLienSoluce=sol;
+        nbLien=0;
     }
 
-    public boolean estValide() {
-        return (etat == etatSoluce);
+    /**
+     * Vérifie si le lien est valide
+     * 
+     * @return True si le lien est valide sinon false
+     */
+    public boolean estValide(){
+        return (nbLien==nbLienSoluce);
     }
 
-    public void lienActiver() {
-        if ((etat += 1) % 3 == 0) {
+    /**
+     * Active le lien le faisant passer a son état suivant
+     * et met a jour le degré actuelle des noeud liés
+     */
+    public void lienActiver(){
+        if((nbLien+=1)%3==0){
             noeud1.enleverDegre();
             noeud2.enleverDegre();
-        } else {
+        }
+        else{
             noeud1.ajouterDegre();
             noeud2.ajouterDegre();
         }
     }
-
+    /**
+     * Récupère le premier noeud
+     * 
+     * @return le premier noeud
+     */
     public Noeud getNoeud1() {
         return noeud1;
     }
 
+    /**
+     * Récupère le deuxième noeud
+     * 
+     * @return le deuxième noeud
+     */
     public Noeud getNoeud2() {
         return noeud2;
     }
 
-    public int getEtat() {
-        return etat;
+    /**
+     * Récupère le nombre de lien
+     * 
+     * @return le nombre de lien
+     */
+    public int getnbLien() {
+        return nbLien;
     }
+
+    
 }
--- src/main/java/fr/m3acnl/game/logique/Noeud.java
+++ src/main/java/fr/m3acnl/game/logique/Noeud.java
@@ -1,36 +1,73 @@
 package fr.m3acnl.game.logique;
 
-public class Noeud {
+import java.util.Hashtable;
 
+/**
+ * Cette classe représente un Noeud dans le jeu du hachi
+ * un noeud connait ça position, son degré actuelle, le degré de ça solution
+ * et une liste d'adjacence pour la connexité.
+ * Il permet l'incrémentation et la decrémentation de son degré actuelle,
+ * si il est valide ainsi que de récupérer ça position et son degré solution
+ */
+public class Noeud{
     private Coord position;
     private int degreSoluce;
     private int degreActuelle;
-    private int listeAdjacence[][];
+    private Hashtable listeAdjacence;
 
-    public Noeud(int x, int y, int degS) {
-        position = new Coord(x, y);
-        degreSoluce = degS;
-        degreActuelle = 0;
+    /**
+     * Constructeur pour créer une nouvelle instance d'un Noeud
+     * 
+     * @param x la coordonnée x du noeud
+     * @param y la coordonnée y du noeud
+     * @param degS le degré solution du noeud
+     */
+    public Noeud(int x, int y, int degS){
+        position=new Coord(x, y);
+        degreSoluce=degS;
+        degreActuelle=0;
+        listeAdjacence=new Hashtable<>();
     }
 
-    public void ajouterDegre() {
-        degreActuelle += 1;
-        return;
+    /**
+     * Incrémente le degré actuelle du noeud
+     */
+    public void ajouterDegre(){
+        degreActuelle+=1;
     }
 
-    public void enleverDegre() {
-        degreActuelle -= 2;
+    /**
+     * Décrémente le degré actuelle du noeud
+     */
+    public void enleverDegre(){
+        degreActuelle-=2;
     }
 
-    public int estValide() {
-        return (degreSoluce - degreActuelle);
+    /**
+     * Vérifie si le noeud est valide
+     * 
+     * @return 0 si valide
+     */
+    public int estValide(){
+        return (degreSoluce-degreActuelle);
     }
 
+    /**
+     * Récupère la position du noeud
+     * 
+     * @return les coordonnées du noeud
+     */
     public Coord getPosition() {
         return position;
     }
 
+    /**
+     * Récupère le degré solution
+     * 
+     * @return le degré solution
+     */
     public int getDegreSoluce() {
         return degreSoluce;
     }
+
 }
