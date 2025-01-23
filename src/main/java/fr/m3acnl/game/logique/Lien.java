011cc47f356e2d49304691b2b8a76d9712c17f45
Yun <aymericmabire@gmail.com>
2025-01-23 08:16:09 +0100

    doc : Remise en forme des commentaires

--- src/main/java/fr/m3acnl/game/logique/Lien.java
+++ src/main/java/fr/m3acnl/game/logique/Lien.java
@@ -1,11 +1,11 @@
 package fr.m3acnl.game.logique;
 
 /**
- * Cette classe représente un lien entre 2 noeud,
- * il connait les 2 noeud lié, son nombre de lien
- * et son nombre de lien soluce
+ * Cette classe représente un lien entre deux noeuds. Il connait les deux noeuds
+ * lié, son nombre de lien et son nombre de lien de la solution
  */
 public class Lien {
+
     private Noeud noeud1;
     private Noeud noeud2;
     private int nbLien;
@@ -13,44 +13,44 @@ public class Lien {
 
     /**
      * Constructeur pour une nouvelle instance de Lien
-     * 
+     *
      * @param n1 premier Noeud
      * @param n2 deuxième Noeud
-     * @param sol le nombre de lien soluce
+     * @param sol le nombre de lien de la solution
      */
-    public Lien(Noeud n1, Noeud n2, int sol){
-        noeud1=n1;
-        noeud2=n2;
-        nbLienSoluce=sol;
-        nbLien=0;
+    public Lien(Noeud n1, Noeud n2, int sol) {
+        noeud1 = n1;
+        noeud2 = n2;
+        nbLienSoluce = sol;
+        nbLien = 0;
     }
 
     /**
      * Vérifie si le lien est valide
-     * 
+     *
      * @return True si le lien est valide sinon false
      */
-    public boolean estValide(){
-        return (nbLien==nbLienSoluce);
+    public boolean estValide() {
+        return (nbLien == nbLienSoluce);
     }
 
     /**
-     * Active le lien le faisant passer a son état suivant
-     * et met a jour le degré actuelle des noeud liés
+     * Active le lien le faisant passer a son état suivant et met a jour le
+     * degré actuelle des noeud liés
      */
-    public void lienActiver(){
-        if((nbLien+=1)%3==0){
+    public void lienActiver() {
+        if ((nbLien += 1) % 3 == 0) {
             noeud1.enleverDegre();
             noeud2.enleverDegre();
-        }
-        else{
+        } else {
             noeud1.ajouterDegre();
             noeud2.ajouterDegre();
         }
     }
+
     /**
      * Récupère le premier noeud
-     * 
+     *
      * @return le premier noeud
      */
     public Noeud getNoeud1() {
@@ -59,7 +59,7 @@ public class Lien {
 
     /**
      * Récupère le deuxième noeud
-     * 
+     *
      * @return le deuxième noeud
      */
     public Noeud getNoeud2() {
@@ -68,7 +68,7 @@ public class Lien {
 
     /**
      * Récupère le nombre de lien
-     * 
+     *
      * @return le nombre de lien
      */
     public int getnbLien() {
@@ -76,12 +76,12 @@ public class Lien {
     }
 
     /**
-     * Vérifie si un noeud voulu est présent dans ce lien
-     * 
+     * Vérifie si un noeud n est présent dans ce lien
+     *
      * @param n Le noeud a vérifier
      * @return Le résultat de la vérification
      */
-    public int noeudDansLien(Noeud n){
-        return n.compareTo(this.noeud1)+n.compareTo(this.noeud2);
-    }    
+    public int noeudDansLien(Noeud n) {
+        return n.compareTo(this.noeud1) + n.compareTo(this.noeud2);
+    }
 }
--- src/main/java/fr/m3acnl/game/logique/Noeud.java
+++ src/main/java/fr/m3acnl/game/logique/Noeud.java
@@ -3,13 +3,12 @@ package fr.m3acnl.game.logique;
 import java.util.Hashtable;
 
 /**
- * Cette classe représente un Noeud dans le jeu du hachi
- * un noeud connait ça position, son degré actuelle, le degré de ça solution
- * et une liste d'adjacence pour la connexité.
- * Il permet l'incrémentation et la decrémentation de son degré actuelle,
- * si il est valide ainsi que de récupérer ça position et son degré solution
+ * Cette classe représente un Noeud dans le jeu du hachi. Un noeud connait: sa
+ * position, son degré actuelle, le degré de sa solution et une liste
+ * d'adjacence stockant les noeuds auquel il est relié directement.
  */
-public class Noeud implements Comparable<Noeud>{
+public class Noeud implements Comparable<Noeud> {
+
     private Coord position;
     private int degreSoluce;
     private int degreActuelle;
@@ -17,44 +16,44 @@ public class Noeud implements Comparable<Noeud>{
 
     /**
      * Constructeur pour créer une nouvelle instance d'un Noeud
-     * 
+     *
      * @param x la coordonnée x du noeud
      * @param y la coordonnée y du noeud
      * @param degS le degré solution du noeud
      */
-    public Noeud(int x, int y, int degS){
-        position=new Coord(x, y);
-        degreSoluce=degS;
-        degreActuelle=0;
-        listeAdjacence=new Hashtable<>();
+    public Noeud(int x, int y, int degS) {
+        position = new Coord(x, y);
+        degreSoluce = degS;
+        degreActuelle = 0;
+        listeAdjacence = new Hashtable<>();
     }
 
     /**
      * Incrémente le degré actuelle du noeud
      */
-    public void ajouterDegre(){
-        degreActuelle+=1;
+    public void ajouterDegre() {
+        degreActuelle += 1;
     }
 
     /**
      * Décrémente le degré actuelle du noeud
      */
-    public void enleverDegre(){
-        degreActuelle-=2;
+    public void enleverDegre() {
+        degreActuelle -= 2;
     }
 
     /**
      * Vérifie si le noeud est valide
-     * 
+     *
      * @return 0 si valide
      */
-    public int estValide(){
-        return (degreSoluce-degreActuelle);
+    public int estValide() {
+        return (degreSoluce - degreActuelle);
     }
 
     /**
      * Récupère la position du noeud
-     * 
+     *
      * @return les coordonnées du noeud
      */
     public Coord getPosition() {
@@ -63,21 +62,21 @@ public class Noeud implements Comparable<Noeud>{
 
     /**
      * Récupère le degré solution
-     * 
+     *
      * @return le degré solution
      */
     public int getDegreSoluce() {
         return degreSoluce;
     }
-    
+
     /**
-     * Comparaison entre 2 noeud
-     * 
+     * Comparaison entre deux noeuds Les noeuds sont comparé par leur position.
+     *
      * @param n2 le noeud avec qui comparé
      * @return le résultat de la comparaison
      */
     @Override
-    public int compareTo(Noeud n2){
+    public int compareTo(Noeud n2) {
         return this.position.compareTo(n2.position);
     }
 }
