942b536849f2a96bcd8f2f3890124b75547a5de3
Ge0mega <cognardluka@gmail.com>
2025-02-02 17:00:15 +0100

    test: Ajout de test

---
+++ src/test/java/fr/m3acnl/game/logique/DoubleLienTest.java
@@ -0,0 +1,94 @@
+package fr.m3acnl.game.logique;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import fr.m3acnl.Tests;
+
+public class DoubleLienTest extends Tests{
+
+    /**
+     * Méthode d'initialisation de la classe de test
+     * @see Tests#printNameAtStart
+     */
+    @BeforeAll
+    public static void initAll(){
+        printNameAtStart(DoubleLienTest.class);
+    }
+
+    /**
+     * Méthode de de fin de la classe de test
+     * @see Tests#printNameAtEnd
+     */
+    @AfterAll
+    public static void endAll(){
+        printNameAtEnd(DoubleLienTest.class);
+    }
+
+    /**
+     * Test de la méthode activeInterrupteur
+     * @see DoubleLien#activeInterrupteur()
+     * @see DoubleLien#getInterupteur()
+     */
+    @Test
+    void testActiveInterrupteur() {
+        DoubleLien dl=new DoubleLien(new Lien(new Noeud(5, 2, 1), new Noeud(5, 4, 1), 1),
+                      new Lien(new Noeud(4, 3, 1), new Noeud(6, 3, 1), 1));
+        dl.activeInterrupteur();
+        assertEquals(true, dl.getInterupteur(),"Interrupteur activer a true");
+        
+    }
+
+    /**
+     * Test de la méthode activeLien
+     * @see DoubleLien#activeLien(Noeud)
+     * @see Noeud#estValide()
+     */
+    @Test
+    void testActiveLien() {
+        Noeud n1 = new Noeud(5, 2, 1);
+        Noeud n2 = new Noeud(5, 4, 1);
+        Noeud n3 = new Noeud(4, 3, 1);
+        Noeud n4 = new Noeud(6, 3, 1);
+        DoubleLien dl=new DoubleLien(new Lien(n1,n2, 1),new Lien(n3,n4, 1));
+        dl.activeLien(n1);
+        dl.activeLien(n2);
+        assertEquals(-1, n1.estValide(),"Lien1 activer 2 fois le noeud lié a -1 deg soluce 1");
+        dl.activeLien(n3);
+        assertEquals(1, n4.estValide(),"Aucune activationdu lien2, Lien1 déja actif renvoie 1");
+        dl.activeLien(n2);
+        dl.activeLien(n3);
+        dl.activeLien(n4);
+        assertEquals(-1, n4.estValide(),"Lien2 activer 2 fois le noeud lié a -1 deg soluce 1");
+
+    }
+
+    /**
+     * Test de la méthode desactiveInterrupteur
+     * @see DoubleLien#activeInterrupteur()
+     * @see DoubleLien#desactiveInterrupteur()
+     * @see DoubleLien#getInterupteur()
+     */
+    @Test
+    void testDesactiveInterrupteur() {
+        DoubleLien dl=new DoubleLien(new Lien(new Noeud(5, 2, 1), new Noeud(5, 4, 1), 1),
+                      new Lien(new Noeud(4, 3, 1), new Noeud(6, 3, 1), 1));
+        dl.activeInterrupteur();
+        dl.desactiveInterrupteur();
+        assertEquals(false, dl.getInterupteur(),"Interrupteur désactiver a false");
+    }
+
+    /**
+     * Test de la méthode getInterrupteur()
+     * @see DoubleLien#getInterupteur()
+     */
+    @Test
+    void testGetInterupteur() {
+        DoubleLien dl=new DoubleLien(new Lien(new Noeud(5, 2, 1), new Noeud(5, 4, 1), 1),
+                      new Lien(new Noeud(4, 3, 1), new Noeud(6, 3, 1), 1));
+        assertEquals(false, dl.getInterupteur(),"Interrupteur de base desactiver a false");
+    }
+}
