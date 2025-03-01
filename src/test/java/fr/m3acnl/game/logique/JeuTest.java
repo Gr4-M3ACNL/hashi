60dde0fa2b05523a6b60c282b6ef6dd911e1119d
meo <meo.prn@outlook.fr>
2025-03-01 18:11:39 +0100

    docs: ajustement de la doc

--- src/test/java/fr/m3acnl/game/logique/JeuTest.java
+++ src/test/java/fr/m3acnl/game/logique/JeuTest.java
@@ -2,7 +2,13 @@ package fr.m3acnl.game.logique;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import fr.m3acnl.Tests;
+import fr.m3acnl.managers.JsonManagerTest;
 
 /**
  * Classe test de la classe Jeu.
@@ -14,7 +20,32 @@ import org.junit.jupiter.api.Test;
  *
  * @author COGNARD Luka
  */
-public class JeuTest {
+public class JeuTest extends Tests {
+
+    /**
+     * Méthode d'initialisation de la classe de test
+     * 
+     * @see Tests#printNameAtStart
+     */
+    @BeforeAll
+    public static void initAll() {
+        printNameAtStart(JeuTest.class);
+    }
+
+    /**
+     * Méthode de fin de la classe de test
+     * 
+     * @see Tests#printNameAtEnd
+     */
+    @AfterAll
+    public static void endAll() {
+        printNameAtEnd(JeuTest.class);
+    }
+
+    /**
+     * Constructeur de la classe de test.
+     */
+    public JeuTest() {}
 
     /**
      * Test de la méthode activeElemJeu.
