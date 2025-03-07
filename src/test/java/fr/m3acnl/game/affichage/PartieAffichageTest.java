6a862524d7da5a5c8532a4861eb5efd77e2f5b6e
meo <meo.prn@outlook.fr>
2025-03-07 17:04:17 +0100

    feat: test vide pour partie affichege

---
+++ src/test/java/fr/m3acnl/game/affichage/PartieAffichageTest.java
@@ -0,0 +1,15 @@
+package fr.m3acnl.game.affichage;
+
+/**
+ * Classe de test de la classe PartieAffichage.
+ * (Vide car la classe PartieAffichage est une interface)
+ */
+public class PartieAffichageTest {
+
+    /**
+     * Constructeur de la classe de test de la classe PartieAffichage.
+     */
+    PartieAffichageTest() {}
+
+     // Aucun test à effectuer car la classe PartieAffichage est une interface 
+}
