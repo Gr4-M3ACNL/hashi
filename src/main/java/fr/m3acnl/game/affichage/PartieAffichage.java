50b99aec049cf285093b3950879dec68bcee2505
Maelig <pyxis@MacBook-Pro-de-Maelig.local>
2025-03-12 11:29:27 +0100

    fix: modification taille assets pour performances 2

--- src/main/java/fr/m3acnl/game/affichage/PartieAffichage.java
+++ src/main/java/fr/m3acnl/game/affichage/PartieAffichage.java
@@ -373,7 +373,6 @@ public class PartieAffichage extends Application {
      * @return L'ImageView créé
      */
     private ImageView creerImageView(URL resource, double size) {
-        System.out.println();
         ImageView imageView = new ImageView(new Image(resource.toExternalForm(),  500 * 0.5, 500 * 0.5, true, true));
         imageView.setFitWidth(size);
         imageView.setFitHeight(size);
