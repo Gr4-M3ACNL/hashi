42d0cd34ef889e72f01ebdfa9cea3f32763aed23
meo <meo.prn@outlook.fr>
2025-03-25 18:24:41 +0100

    feat: ajout des package info

---
+++ src/main/java/fr/m3acnl/game/logique/elementjeu/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Package contenant les éléments servant à représenter une grille de hashi.
+ * <br>
+ * <br>
+ * <a href="javascript:void(0);" onclick="toggleDiagram()" style="padding:5px 10px; background-color:#007BFF; color:white; text-decoration:none; border-radius:5px;">Afficher/Cacher le diagramme de classe</a>
+ * <div id="diagram" style="display:none;">
+ *   <img src="doc-files/elementjeu.svg" alt="Diagramme de classe du package elementjeu" width="75%"/>
+ * </div>
+ * <script>
+ *   function toggleDiagram() {
+ *     var diagram = document.getElementById('diagram');
+ *     if (diagram.style.display === 'none') {
+ *       diagram.style.display = 'block';
+ *     } else {
+ *       diagram.style.display = 'none';
+ *     }
+ *   }
+ * </script>
+ */
+
+package fr.m3acnl.game.logique.elementjeu;
---
+++ src/main/java/fr/m3acnl/profile/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Package contenant la gestion des profils de joueur.
+ * <br>
+ * la gestion du profil des paramètre et de l'historique de jeu son séparé dans différents classes
+ * <br>
+ * <br>
+ * <a href="javascript:void(0);" onclick="toggleDiagram()" style="padding:5px 10px; background-color:#007BFF; color:white; text-decoration:none; border-radius:5px;">Afficher/Cacher le diagramme de classe</a>
+ * <div id="diagram" style="display:none;">
+ *   <img src="doc-files/profile.svg" alt="Diagramme de classe du package profile" width="75%"/>
+ * </div>
+ * <script>
+ *   function toggleDiagram() {
+ *     var diagram = document.getElementById('diagram');
+ *     if (diagram.style.display === 'none') {
+ *       diagram.style.display = 'block';
+ *     } else {
+ *       diagram.style.display = 'none';
+ *     }
+ *   }
+ * </script>
+ */
+
+package fr.m3acnl.profile;
