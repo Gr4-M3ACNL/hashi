360f3ff87e8b6b994a60be1761e305afd46c3329
Maelig <pyxis@MacBook-Pro-de-Maelig.local>
2025-03-31 15:15:45 +0200

    docs: correction javadoc

--- src/main/java/fr/m3acnl/game/logique/aide/package-info.java
+++ src/main/java/fr/m3acnl/game/logique/aide/package-info.java
@@ -4,7 +4,7 @@
  * <br>
  * <a href="javascript:void(0);" onclick="toggleDiagram()" style="padding:5px 10px; background-color:#007BFF; color:white; text-decoration:none; border-radius:5px;">Afficher/Cacher le diagramme de classe</a>
  * <div id="diagram" style="display:none;">
- *   <img src="doc-files/aide.svg" alt="Diagramme de classe du package game.logique.aide" width="75%"/>
+ *   <img src="doc-files/aides.svg" alt="Diagramme de classe du package game.logique.aide" width="75%"/>
  * </div>
  * <script>
  *   function toggleDiagram() {
